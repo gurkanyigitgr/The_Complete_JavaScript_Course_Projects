47acebded06739ba5281245f2b401cbd8c04c3d9
Gürkan <gurkanyigitgr@gmail.com>
2023-04-11 16:03:39 +0300

    completed alarm clock

--- 04. Alarm_Clock/script.js
+++ 04. Alarm_Clock/script.js
@@ -7,20 +7,70 @@ let alarmTime,
   isAlarmSet,
   ringtone = new Audio("./assets/alarm_ring.mp3");
 
+let amPm;
+
 for (i = 12; i > 0; i--) {
-  i < 10 ? `0${i}` : i;
+  i = i < 10 ? `0${i}` : i;
   let option = `<option value="${i}">${i}</option> `;
   selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
 }
 
 for (i = 59; i > 0; i--) {
-  i < 10 ? `0${i}` : i;
+  i = i < 10 ? `0${i}` : i;
   let option = `<option value="${i}">${i}</option>`;
   selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
 }
 
 for (i = 2; i > 0; i--) {
-  let amPm = i == 1 ? "AM" : "PM";
+  amPm = i == 1 ? "AM" : "PM";
   let option = `<option value="${amPm}">${amPm}</option>`;
   selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
 }
+
+setInterval(() => {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  amPm = "AM";
+  if (h >= 12) {
+    h = h - 12;
+    amPm = "PM";
+  }
+
+  h = h === 0 ? "12" : h;
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  currentTime.innerText = `${h}:${m}:${s} ${amPm}`;
+  if (alarmTime === `${h}:${m} ${amPm}`) {
+    ringtone.play();
+    ringtone.loop = true;
+  }
+});
+
+function setAlarm() {
+  if (isAlarmSet) {
+    alarmTime = "";
+    ringtone.pause();
+    content.classList.remove("disable");
+    setAlarmBtn.innerText = "Set Alarm";
+    return (isAlarmSet = false);
+  }
+
+  let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
+  if (
+    time.includes("Hour") ||
+    time.includes("Minute") ||
+    time.includes("AM/PM")
+  ) {
+    return alert("Please, select a valid time to set Alarm!");
+  }
+  alarmTime = time;
+  isAlarmSet = true;
+  content.classList.add("disable");
+  setAlarmBtn.innerText = "Clear Alarm";
+}
+
+setAlarmBtn.addEventListener("click", setAlarm);
